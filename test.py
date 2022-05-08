918c333e2b3072095f538105210ee16805d61396
Yaminik1996 <yaminik1996@gmail.com>
2022-05-08 17:05:15 -0400

    updating normalization

--- test.py
+++ test.py
@@ -34,20 +34,25 @@ def test_model(model, best_results_dir, classes, batch_size):
     
     with torch.no_grad(): 
         
-        data = np.load("bed0.points.ply.npy")    
-        data = torch.tensor(data).unsqueeze(0)
-        data = data.float()
-        data_0 = data.transpose(2,1).to(device)
+        data0 = np.load("bed0.points.ply.npy")    
+        data0 = torch.tensor(data0).unsqueeze(0)
+        data0 = data0.float()
         
         
-        data = np.load("bed1.points.ply.npy")    
-        data = torch.tensor(data).unsqueeze(0)
-        data = data.float()
-        data_1 = data.transpose(2,1).to(device)
-
+        
+        data1 = np.load("bed1.points.ply.npy")    
+        data1 = torch.tensor(data1).unsqueeze(0)
+        data1 = data1.float()
+        print(data1.shape)
+        
         normalization_function = pointcloud_processor.Normalization.normalize_bounding_box_functional
         data_0[:, :3] = normalization_function(data_0[:, :3])
         data_1[:, :3] = normalization_function(data_1[:, :3])
+        
+        data_0 = data.transpose(2,1).to(device)
+        data_1 = data.transpose(2,1).to(device)
+        
+        print(data1.shape)
 
         outputs = model(data_0, data_1, train=False)
 
