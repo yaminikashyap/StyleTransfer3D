c2ac1c4d2b0710984391dddbfa3a47b5799325e2
Jui Shah <34505328+shahjui2000@users.noreply.github.com>
2022-05-08 15:23:47 -0400

    bash script for test

---
+++ test.sh
@@ -0,0 +1,34 @@
+DATA_DIR="/mnt/nfs/work1/mccallum/jbshah/3dsnet/dataset/data"
+#DATA_DIR="/mnt/nfs/scratch1/nikhilagarwa/3dsnet/dataset/data/"
+RESULTS_DIR="/mnt/nfs/work1/mccallum/jbshah/3dsnet/dataset/data/best_results/chair"
+GENERATOR_NORM="bn"
+DISCRIMINATOR_NORM="bn"
+NUM_LAYERS=2
+W_CHAMFER=1.0
+W_CYCLE_CHAMFER=0.1
+W_ADVERSARIAL=1.0
+W_PERCEPTUAL=0.0
+W_CONTENT_REC=0.1
+W_STYLE_REC=0.1
+NUMBER_POINTS=2500
+BATCH_SIZE=1
+GEN_LR=0.001
+DIS_LR=0.004
+NEPOCH=180
+
+srun --partition=2080ti-long --gres=gpu:1 python test.py \
+--data_dir=$DATA_DIR \
+--family "chair" \
+--class_0 "armchair" \
+--class_1 "straight chair,side chair" \
+--batch_size=$BATCH_SIZE \
+--weight_chamfer=$W_CHAMFER \
+--weight_cycle_chamfer=$W_CYCLE_CHAMFER \
+--weight_adversarial=$W_ADVERSARIAL \
+--weight_perceptual=$W_PERCEPTUAL \
+--weight_content_reconstruction=$W_CONTENT_REC \
+--weight_style_reconstruction=$W_STYLE_REC \
+--nepoch=$NEPOCH \
+--generator_lrate=$GEN_LR \
+--discriminator_lrate=$DIS_LR \
+--best_results=$RESULTS_DIR \
